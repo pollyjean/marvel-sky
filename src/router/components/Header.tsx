caed1b902a67347b1595dbe391994cf6f50fadcf
sky(skysurfr) <skysurfr@me.com>
2023-09-10 08:15:28 +0900

    Chore: 로딩화면, 메인 상단 링크 작동 수정

--- src/router/components/Header.tsx
+++ src/router/components/Header.tsx
@@ -1,10 +1,12 @@
 import { Link } from "react-router-dom";
 import styles from "./Header.module.css";
+import { useLocation } from "react-router-dom";
 
 const Header = () => {
+  const { pathname } = useLocation();
   return (
     <div className={styles.Wrap}>
-      <Link to="/" className={styles.Link}>
+      <Link to={pathname === "/" ? "/#main" : "/"} className={styles.Link}>
         <h1 className={styles.Logo}>
           <img src={"/marvel_logo.jpg"} className={styles.LogoImage} alt="Marvel Logo" />{" "}
           <span className={styles.LogoText}>
